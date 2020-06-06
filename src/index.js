b57d18628e717ebbbfaeb97ffecdbf5b55945abf
Ojhal Patwa <patkarojhal26@gmail.com>
2020-06-06 14:56:23 +0530

    create component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import  React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-let el = <h1>Hello React</h1>;
-ReactDOM.render(el, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
