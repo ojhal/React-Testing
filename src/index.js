836a8774cd4d99f2784c037ac0de18dc742c13c8
Ojhal Patwa <patkarojhal26@gmail.com>
2020-06-07 16:37:06 +0530

    Use props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import  React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Student from "./Student";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Student  name="Ojhal" />, document.getElementById("root"));
