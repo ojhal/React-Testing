2e4cf88771ba4c54c64b3c13975eebd449650504
Ojhal Patwa <patkarojhal26@gmail.com>
2020-06-14 23:42:45 +0530

    Use destructuring

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
 function Hooks() {
-  const nameStateVariable = useState("Ojhal");
+  const [name, setName] = useState("Ojhal");
+  const [roll, setRoll] = useState(101);
+  // const name = nameStateVariable[0];
+  // const setName = nameStateVariable[1];
+  const handleClick = () => {
+    setName("Rajat");
+    setRoll(456);
+  }
   console.log("i am leaner");
   return (
     <React.Fragment>
 
-      <h1>{nameStateVariable[0]}</h1>
+      <h1>Name:{name}</h1>
+      <h2>Roll:{roll}</h2>
+      <button type="button" onClick={handleClick}>Change</button>
     </React.Fragment>
   );
 }
