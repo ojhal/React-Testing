c5fbf08a8b99c5144d842356f5daba8f14dc3a5e
Ojhal Patwa <patkarojhal26@gmail.com>
2020-06-15 20:40:06 +0530

    I learned useEffect

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,21 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Hooks() {
-  const [name, setName] = useState("Ojhal");
-  const [roll, setRoll] = useState(101);
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(50);
+
+  // const [roll, setRoll] = useState(101);
   // const name = nameStateVariable[0];
   // const setName = nameStateVariable[1];
-  const handleClick = () => {
-    setName("Rajat");
-    setRoll(456);
-  }
-  console.log("i am leaner");
+  const handleIncrement = () => {
+    setCount(count + 1);
+    // setRoll(456);
+  };
+
+  const handleDecrement = () => {
+    setCount1(count1 - 1);
+
+  };
+  useEffect(() => {
+    console.log("use effect called");
+
+  }, [count1]);
+
+  // console.log("i am leaner");
   return (
     <React.Fragment>
 
-      <h1>Name:{name}</h1>
-      <h2>Roll:{roll}</h2>
-      <button type="button" onClick={handleClick}>Change</button>
+      <h1>Count UP:{count}</h1>
+      {/* <h2>Roll:{roll}</h2> */}
+      <button type="button" onClick={handleIncrement}>Increment</button>
+
+      <h1>Count Down:{count1}</h1>
+      {/* <h2>Roll:{roll}</h2> */}
+      <button type="button" onClick={handleDecrement}>Decrement</button>
     </React.Fragment>
   );
 }
