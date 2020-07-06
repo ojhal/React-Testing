9553fffc0e29d4f56c679ecf4b4b4269edadf61f
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-06 23:23:55 +0530

    I learned lists

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ export default class App extends Component {
     const arr = [10, 20, 30, 40];
     return (
       <ul>
-        <li>{arr[0]}</li>
+        <li>{arr[0] * 2}</li>
+        <li>{arr[1] * 2}</li>
+        <li>{arr[2] * 2}</li>
+        <li>{arr[3] * 2}</li>
       </ul>
     )
 
