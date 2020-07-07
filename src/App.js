64fb1b41eb0e5d7b20af5d0301a2cfeacbcae341
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-07 23:33:13 +0530

    Using key

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import React, { Component } from 'react'
 
 
 export default class App extends Component {
+  state = {
+    users: [
+      { id: 101, name: "Rahul", password: "3245gfgf" },
+      { id: 102, name: "Rahul1", password: "3245gfjkl" },
+      { id: 103, name: "Rahul2", password: "3245gfghj" },
+    ],
+    isLoggedIn: false
+  }
   render() {
-    const arr = [10, 20, 30, 40];
-    const newArr = arr.map((num) => {
-      console.log("Num = ", num);
-      return <li>{num * 2}</li>
+    const newUsers = this.state.users.map(user => {
+      console.log(user);
+      return (
+        <h1 key={user.id}>
+          ID: {user.id} Name: {user.name} password: {user.password}
+        </h1>
+      );
     });
-    console.log("oldArray = ", arr);
-    console.log("NewArray = ", newArr);
-
-
     return (
-      <ul>
-        {newArr}
-      </ul>
+      <div>{newUsers}</div>
     )
 
 
