878d97e166124d3978d6917ca987841e4927c3f7
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-07 16:43:37 +0530

    use lists

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ export default class App extends Component {
   render() {
     const arr = [10, 20, 30, 40];
     const newArr = arr.map((num) => {
+      console.log("Num = ", num);
       return <li>{num * 2}</li>
-    })
+    });
+    console.log("oldArray = ", arr);
+    console.log("NewArray = ", newArr);
 
 
     return (
       <ul>
-        {newarr}
+        {newArr}
       </ul>
     )
 
