8ef804605af4931acf7ed651318337841c1fdb6c
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-08 16:38:09 +0530

    Using inline css

--- src/App.js
+++ src/App.js
@@ -4,30 +4,44 @@ import React, { Component } from 'react'
 
 
 export default class App extends Component {
-  state = {
-    users: [
-      { id: 101, name: "Rahul", password: "3245gfgf" },
-      { id: 102, name: "Rahul1", password: "3245gfjkl" },
-      { id: 103, name: "Rahul2", password: "3245gfghj" },
-    ],
-    isLoggedIn: false
-  }
   render() {
-    const newUsers = this.state.users.map(user => {
-      console.log(user);
-      return (
-        <h1 key={user.id}>
-          ID: {user.id} Name: {user.name} password: {user.password}
-        </h1>
-      );
-    });
+    const btnStyle = {
+      color: "blue",
+      backgroundColor: "orange"
+    };
     return (
-      <div>{newUsers}</div>
+      <div>
+        <button style={btnStyle}>button</button>
+
+      </div>
+
     )
-
-
   }
 }
+//   state = {
+//     users: [
+//       { id: 101, name: "Rahul", password: "3245gfgf" },
+//       { id: 102, name: "Rahul1", password: "3245gfjkl" },
+//       { id: 103, name: "Rahul2", password: "3245gfghj" },
+//     ],
+//     isLoggedIn: false
+//   }
+//   render() {
+//     const newUsers = this.state.users.map(user => {
+//       console.log(user);
+//       return (
+//         <h1 key={user.id}>
+//           ID: {user.id} Name: {user.name} password: {user.password}
+//         </h1>
+//       );
+//     });
+//     return (
+//       <div>{newUsers}</div>
+//     )
+
+
+//   }
+// }
 
 
 
