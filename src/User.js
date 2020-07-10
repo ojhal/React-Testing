d165e943296f720b664b599e11f151c8437a8fb6
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-10 20:10:31 +0530

    Using module css

--- src/User.js
+++ src/User.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Styles from "./User.module.css";
+import Styles1 from "./App.module.css";
 export default class User extends Component {
   render() {
-    return <h1 className={Styles.txt}>Hello User</h1>
+    return (<div>
+      <h2 className={Styles.txt}>Hello User</h2>
+      <h2 className={Styles1.txt}>Hello User 1</h2>
+    </div>
+    );
   }
 }
