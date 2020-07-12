68865e8ac29a66bcf6cd906f3a62ab9391087658
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-12 15:11:57 +0530

    Using refs

--- src/App.js
+++ src/App.js
@@ -74,24 +74,31 @@ import React, { Component } from 'react'
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: ""
+
+    };
 
     this.textInput = React.createRef();
   }
-  componentDidMount = () => {
-    // console.log(this.textInput.current)
-    this.textInput.current.focus();
-  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.textInput.current.value)
+    this.setState({ value: this.textInput.current.value })
+  };
+
   render() {
     return (
-      <form>
-        Name: <input type="text" />
-        <br />
-        <br />
-        password: <input type="text" ref={this.textInput} />
-        <br />
-        <br />
-        Address: <input type="text" />
-      </form>
+      <React.Fragment>
+        <h2>You typed: {this.state.value}</h2>
+        <form onSubmit={this.handleSubmit}>
+          Input: <input type="text" ref={this.textInput} />
+          <input type="submit" value="Submit" />
+        </form>
+      </React.Fragment>
+
+
     )
 
   }
