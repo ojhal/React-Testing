618f0937af0d363ad182b2e3127b65022d34607b
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-13 15:40:31 +0530

    Image in public folder

--- src/App.js
+++ src/App.js
@@ -69,40 +69,40 @@
 //   }
 // }
 
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: ""
 
-    };
+//     };
 
-    this.textInput = React.createRef();
-  }
+//     this.textInput = React.createRef();
+//   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // console.log(this.textInput.current.value)
-    this.setState({ value: this.textInput.current.value })
-  };
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     // console.log(this.textInput.current.value)
+//     this.setState({ value: this.textInput.current.value })
+//   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <h2>You typed: {this.state.value}</h2>
-        <form onSubmit={this.handleSubmit}>
-          Input: <input type="text" ref={this.textInput} />
-          <input type="submit" value="Submit" />
-        </form>
-      </React.Fragment>
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h2>You typed: {this.state.value}</h2>
+//         <form onSubmit={this.handleSubmit}>
+//           Input: <input type="text" ref={this.textInput} />
+//           <input type="submit" value="Submit" />
+//         </form>
+//       </React.Fragment>
 
 
-    )
+//     )
 
-  }
-}
+//   }
+// }
 
 
 
