a5f8aabc0b716df36749e9df559fb7877278b656
Ojhal Patwa <patkarojhal26@gmail.com>
2020-07-13 16:03:38 +0530

    Image in src folder

--- src/App.js
+++ src/App.js
@@ -101,8 +101,23 @@
 
 //     )
 
-//   }
-// }
+//   
+
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={process.env.PUBLIC_URL + "/pic.jpg"}
+          alt="Mypic"
+          width="300px"
+        />
+      </div>
+    );
+  }
+}
 
 
 
